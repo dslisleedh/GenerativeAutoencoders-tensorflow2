375e94c3d545616693e6922a284e11983662c0fa
dslisleedh <gnova6kud@gmail.com>
2022-01-06 08:35:11 +0900

    fix dfcvae

--- dfcvae.py
+++ dfcvae.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 
+
 class Encoder(tf.keras.layers.Layer):
     def __init__(self, n_filters, n_layers, dim_latent):
         super(Encoder, self).__init__()
@@ -10,20 +11,21 @@ class Encoder(tf.keras.layers.Layer):
         self.Downsampling = tf.keras.Sequential()
         for i in range(self.n_layers):
             self.Downsampling.add(tf.keras.layers.Conv2D(self.n_filters * (2 ** i),
-                                                         kernel_size = (4,4),
-                                                         strides = (2,2),
-                                                         padding = 'same',
-                                                         activation = 'linear',
-                                                         use_bias = False
+                                                         kernel_size=(4, 4),
+                                                         strides=(2, 2),
+                                                         padding='same',
+                                                         activation='linear',
+                                                         use_bias=False
                                                          )
                                   )
             self.Downsampling.add(tf.keras.layers.BatchNormalization())
             self.Downsampling.add(tf.keras.layers.LeakyReLU())
+        self.Downsampling.add(tf.keras.layers.GlobalAveragePooling2D())
         self.Mean = tf.keras.layers.Dense(self.dim_latent,
-                                          activation = 'linear'
+                                          activation='linear'
                                           )
         self.LogVar = tf.keras.layers.Dense(self.dim_latent,
-                                            activation = 'linear'
+                                            activation='linear'
                                             )
 
     def call(self, X):
@@ -41,50 +43,50 @@ class Decoder(tf.keras.layers.Layer):
         self.n_layers = n_layers
 
         self.FC = tf.keras.layers.Dense(self.n_nodes,
-                                        activation = 'relu',
-                                        kernel_initializer = 'he_normal'
+                                        activation='relu',
+                                        kernel_initializer='he_normal'
                                         )
         self.Upsampling = tf.keras.Sequential()
         for i in range(self.n_layers - 1):
-            self.Upsampling.add(tf.keras.layers.UpSampling2D(size = (2,2),
-                                                             interpolation = 'nearest'
+            self.Upsampling.add(tf.keras.layers.UpSampling2D(size=(2, 2),
+                                                             interpolation='nearest'
                                                              )
                                 )
             self.Upsampling.add(tf.keras.layers.Conv2D(self.n_nodes / (2 ** i),
-                                                       kernel_size = (3,3),
-                                                       activation = 'linear',
-                                                       use_bias = False,
-                                                       padding = 'same'
+                                                       kernel_size=(3, 3),
+                                                       activation='linear',
+                                                       use_bias=False,
+                                                       padding='same'
                                                        )
                                 )
             self.Upsampling.add(tf.keras.layers.BatchNormalization())
             self.Upsampling.add(tf.keras.layers.LeakyReLU())
-        self.Upsampling.add(tf.keras.layers.UpSampling2D(size = (2,2),
-                                                         interpolation = 'nearest'
+        self.Upsampling.add(tf.keras.layers.UpSampling2D(size=(2, 2),
+                                                         interpolation='nearest'
                                                          )
                             )
         self.Upsampling.add(tf.keras.layers.Conv2D(3,
-                                                   kernel_size = (3,3),
-                                                   padding = 'same',
-                                                   activation = 'sigmoid'
+                                                   kernel_size=(3, 3),
+                                                   padding='same',
+                                                   activation='sigmoid'
                                                    )
                             )
 
     def call(self, latent):
         latent = self.FC(latent)
-        latent = tf.reshape(latent, shape = (-1, 4, 4))
+        latent = tf.reshape(latent, shape=(-1, 4, 4, 256))
         reconstruction = self.Upsampling(latent)
         return reconstruction
 
 
 class DFCVAE(tf.keras.models.Model):
     def __init__(self,
-                 n_nodes = 4096,
-                 E_filters = 32,
-                 E_layers = 4,
-                 D_filters = 128,
-                 D_layers = 4,
-                 dim_latent = 100):
+                 n_nodes=4096,
+                 E_filters=32,
+                 E_layers=4,
+                 D_filters=128,
+                 D_layers=4,
+                 dim_latent=100):
         super(DFCVAE, self).__init__()
         self.n_nodes = n_nodes
         self.E_filters = E_filters
@@ -101,4 +103,47 @@ class DFCVAE(tf.keras.models.Model):
                                                 self.vgg.layers[21].output]
                                        )
         self.Encoder = Encoder(self.E_filters, self.E_layers, self.dim_latent)
-        self.Decoder = Decoder()
+        self.Decoder = Decoder(self.n_nodes, self.D_filters, self.D_layers)
+
+    @tf.function
+    def reparameterization(self, mean, logvar):
+        epsilon = tf.random.normal(shape=tf.shape(mean),
+                                   dtype='float32'
+                                   )
+        return (epsilon * tf.exp(.5 * logvar)) + mean
+
+    @tf.function
+    def compute_DFC(self, X, reconstruction):
+        X_output = self.DFCModel(X)
+        recon_output = self.DFCModel(reconstruction)
+        loss = 0
+        for x_, recon_ in zip(X_output, recon_output):
+            loss += tf.reduce_mean(tf.square(x_ - recon_)) / 2
+        return loss
+
+    def compile(self, optimizer):
+        super(DFCVAE, self).compile()
+        self.optimizer = optimizer
+
+    @tf.function
+    def train_step(self, Input):
+        X, _ = Input
+        with tf.GradientTape() as tape:
+            mean, logvar = self.Encoder(X)
+            latent = self.reparameterization(mean, logvar)
+            reconstuction = self.Decoder(latent)
+            reconstruction_loss = tf.reduce_mean(self.compute_DFC(X, reconstuction))
+            kl_loss = tf.reduce_mean(
+                -.5 * tf.reduce_sum((1 + logvar - tf.square(mean) - tf.exp(logvar + 1e-8)), axis=1))
+            ELBO = reconstruction_loss + kl_loss
+        grads = tape.gradient(ELBO, self.Encoder.trainable_variables + self.Decoder.trainable_variables)
+        self.optimizer.apply_gradients(
+            zip(grads, self.Encoder.trainable_variables + self.Decoder.trainable_variables)
+        )
+        return {'reconstruction_loss': reconstruction_loss, 'kl_loss': kl_loss}
+
+    def call(self, X):
+        mean, logvar = self.Encoder(X)
+        latent = self.reparameterization(mean, logvar)
+        reconstuction = self.Decoder(latent)
+        return reconstuction
